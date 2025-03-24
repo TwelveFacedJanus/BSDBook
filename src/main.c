ed3c1fc3f65b8937cfe8c5411af353d67829bffb
TwelveFacedJanus <twofaced-janus@yandex.ru>
2025-03-24 14:00:00 +0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,148 @@
-#include "fs.h"
-#include <stdlib.h> // For system()
-#include <stdio.h>  // For printf()
-#include <string.h> // For strstr()
-#include <dirent.h> // For directory operations
-#include <sys/stat.h> // For stat()
-#include <time.h>   // For ctime()
+#define _XOPEN_SOURCE 500
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <ftw.h>
+#include <errno.h>
+#include <time.h>
 #include <ncurses.h> // For TUI
 
-#include "settings.h"
+#define DEFAULT_BOOKS_PATH "$HOME/books"
+
+char* get_default_books_path() {
+    char* home_dir = getenv("HOME");
+    if (home_dir == NULL) {
+        perror("Unable to get HOME directory");
+        exit(EXIT_FAILURE);
+    }
+    char* default_books_path = malloc(strlen(home_dir) + strlen("/books") + 1);
+    if (default_books_path == NULL) {
+        perror("Unable to allocate memory");
+        exit(EXIT_FAILURE);
+    }
+    sprintf(default_books_path, "%s/books", home_dir);
+    return default_books_path;
+}
+
+int create_note(const char* bookname, const char* notename)
+{
+    char* default_books_path = get_default_books_path();
+    char note_path[1024];
+    snprintf(note_path, sizeof(note_path), "%s/%s/%s.bdsb", default_books_path, bookname, notename);
+
+    char book_path[1024];
+    snprintf(book_path, sizeof(book_path), "%s/%s", default_books_path, bookname);
+
+    free(default_books_path);
+
+    struct stat book_info;
+    if (stat(book_path, &book_info) != 0 || !(book_info.st_mode & S_IFDIR))
+    {
+        printf("Book directory does not exist. Please run 'bsdbook init default'.\n");
+        return -1;
+    }
+
+    if (access(note_path, F_OK) == 0)
+    {
+        printf("Note already exists!\n");
+        return -1;
+    }
+
+    FILE* fp = fopen(note_path, "a");
+    if (fp == NULL)
+    {
+        printf("Error creating Note: %s\n", strerror(errno));
+        return -1;
+    }
+    fclose(fp);
+    printf("Note has been created!\n");
+    return 0;
+}
+
+int create_book(const char* bookname)
+{
+    char* default_books_path = get_default_books_path();
+    char book_path[1024];
+    snprintf(book_path, sizeof(book_path), "%s/%s", default_books_path, bookname);
+
+    free(default_books_path);
+
+    if (mkdir(book_path, 0755) == 0)
+    {
+        printf("Book has been created!\n");
+        return 0;
+    }
+    printf("Failed to create book directory: %s\n", strerror(errno));
+    return -1;
+}
+
+int install_bsdbook()
+{
+    char* default_books_path = get_default_books_path();
+    struct stat info;
+    if (stat(default_books_path, &info) != 0) {
+        printf("Directory does not exist. Creating...\n");
+        if (mkdir(default_books_path, 0755) == 0) {
+            printf("Directory created!\n");
+            free(default_books_path);
+            return 0;
+        }
+        printf("Failed to create directory: %s\n", strerror(errno));
+        free(default_books_path);
+        return -1;
+    } else if (info.st_mode & S_IFDIR) {
+        printf("Directory already exists.\n");
+        free(default_books_path);
+        return -1;
+    }
+    printf("Path exists but is not a directory.\n");
+    free(default_books_path);
+    return -1;
+}
+
+void get_books() {
+    char* default_books_path = get_default_books_path();
+    DIR* dir;
+    struct dirent* entry;
+    struct stat statbuf;
+
+    dir = opendir(default_books_path);
+    if (!dir) {
+        perror("opendir");
+        free(default_books_path);
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+        char fullpath[1024];
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", default_books_path, entry->d_name);
+
+        if (stat(fullpath, &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
+            printf("%s\n", entry->d_name);
+        }
+    }
+    closedir(dir);
+    free(default_books_path);
+}
+
+int unlink_cb(const char* fpath, const struct stat *sb, int typeflag, struct FTW* ftwbuf) {
+    int rv = remove(fpath);
+    if (rv)
+        perror("ERROR unlinking cb.\n");
+    return rv;
+}
+
+int delete_folder_recursive(const char* fpath) {
+    return nftw(fpath, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+}
 
 void show_welcome_and_help() {
     printf("Welcome to BSDNotes!\n");
@@ -39,16 +174,18 @@ int is_regular_file(const char *path) {
 }
 
 void show_todos() {
+    char* default_books_path = get_default_books_path();
     DIR *books_dir = opendir(default_books_path);
     if (!books_dir) {
         perror("Unable to open 'books' directory");
+        free(default_books_path);
         return;
     }
 
     struct dirent *book_entry;
     while ((book_entry = readdir(books_dir))) {
         char book_path[1024];
-        snprintf(book_path, sizeof(book_path), "%s/%s", default_books_path ,book_entry->d_name);
+        snprintf(book_path, sizeof(book_path), "%s/%s", default_books_path, book_entry->d_name);
 
         if (is_directory(book_path) && strcmp(book_entry->d_name, ".") != 0 && strcmp(book_entry->d_name, "..") != 0) {
             DIR *notes_dir = opendir(book_path);
@@ -87,15 +224,18 @@ void show_todos() {
     }
 
     closedir(books_dir);
+    free(default_books_path);
 }
 
 void print_notes_from_book(const char *book_name) {
+    char* default_books_path = get_default_books_path();
     char book_path[1024];
     snprintf(book_path, sizeof(book_path), "%s/%s", default_books_path, book_name);
 
     DIR *notes_dir = opendir(book_path);
     if (!notes_dir) {
         perror("Unable to open book directory");
+        free(default_books_path);
         return;
     }
 
@@ -123,6 +263,7 @@ void print_notes_from_book(const char *book_name) {
     }
 
     closedir(notes_dir);
+    free(default_books_path);
 }
 
 void show_tui_help(WINDOW *win) {
@@ -197,13 +338,17 @@ int main(int argc, char* argv[]) {
         install_bsdbook();
     } else if (argc >= 2 && strcmp(argv[1], "delete") == 0) {
         if (argc >= 4 && strcmp(argv[2], "book") == 0) {
+            char* default_books_path = get_default_books_path();
             char book_path[1024];
             snprintf(book_path, sizeof(book_path), "%s/%s", default_books_path, argv[3]);
+            free(default_books_path);
             if (delete_folder_recursive(book_path) == 0)
                 printf("Book has been deleted!\n");
         } else if (argc >= 5 && strcmp(argv[2], "note") == 0) {
+            char* default_books_path = get_default_books_path();
             char note_path[1024];
             snprintf(note_path, sizeof(note_path), "%s/%s/%s.bdsb", default_books_path, argv[3], argv[4]);
+            free(default_books_path);
             if (remove(note_path) == 0)
                 printf("Note has been deleted!\n");
         }
@@ -222,8 +367,10 @@ int main(int argc, char* argv[]) {
     } else if (strcmp(argv[1], "books") == 0) {
         get_books();
     } else if (strcmp(argv[1], "edit") == 0 && argc >= 4) {
+        char* default_books_path = get_default_books_path();
         char note_path[1024];
         snprintf(note_path, sizeof(note_path), "%s/%s/%s.bdsb", default_books_path, argv[2], argv[3]);
+        free(default_books_path);
         char command[1024];
         snprintf(command, sizeof(command), "nvim %s", note_path);
         system(command); // Open the note in NeoVim
