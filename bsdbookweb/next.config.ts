1702c3ff9728ea1dfaf51cabbe939bf1d4de765d
TwelveFacedJanus <twofaced-janus@yandex.ru>
2025-03-29 18:43:15 +0500

    Update next.config.ts

--- bsdbookweb/next.config.ts
+++ bsdbookweb/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig
